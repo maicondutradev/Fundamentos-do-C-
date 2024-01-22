e306f50a47d5b4faf94b7eb4ad15184af5381e69
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-22 14:07:56 -0300

    Csharp

--- MyApp/obj/Debug/netcoreapp3.1/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/netcoreapp3.1/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60fee9f74812af75621418557ca5eb0d60b33b16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef2c95520da2953099eb6857c0de0111e8fb879")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
