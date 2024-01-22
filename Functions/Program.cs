df8df31d7a5f4a1e691dd27dddba9712aa710571
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-22 14:46:51 -0300

    CSharp

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 
-class Program 
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        
-        
+        MeuMetodo();
+        string nome = RetornaNome("Maicon", "Fonseca Dutra", 23, 78);
+        Console.WriteLine(nome);
+        Console.WriteLine(RetornaNome("Mirela", "Taciane dos Santos", 23, 55));
+    }
+    static void MeuMetodo()
+    {
+        Console.WriteLine("Meu metodo!");
+    }
+    static string RetornaNome(
+    string nome, 
+    string sobrenome, 
+    int idade, 
+    int peso = 0) //parâmetro opcional
+    {
+        return nome + " " + sobrenome + " tem " + idade.ToString() + " anos de idade!" + peso;
     }
 }
