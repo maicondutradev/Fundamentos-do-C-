72e37c511a07761cc13ced1ee382f85c3d21839a
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-23 08:52:15 -0300

    Csharp

---
+++ Structs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(1, "Mouse gamer", 35.99);
+
+            mouse.Id = 44;
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id;
+        public string Name;
+        public double Price;
+
+        public double PrinceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+}
