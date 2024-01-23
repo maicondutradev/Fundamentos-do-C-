cac7aad6028d22ab368256d45b5832280bca9361
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-23 09:18:36 -0300

    CSharp

--- Structs/Program.cs
+++ Structs/Program.cs
@@ -6,32 +6,45 @@ namespace Structs
     {
         static void Main(string[] args)
         {
-            Product mouse = new Product(1, "Mouse gamer", 35.99);
-
+            Product mouse = new Product(1, "Mouse gamer", 35.99,EProductType.Product);
+            var manutencaoEletrica = new Product(2, "Manutenção elétrica residencial", 500, EProductType.Service);
             mouse.Id = 44;
 
             Console.WriteLine(mouse.Id);
             Console.WriteLine(mouse.Name);
             Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+            Console.WriteLine(manutencaoEletrica.Id);
+            Console.WriteLine(manutencaoEletrica.Name);
+            Console.WriteLine(manutencaoEletrica.Price);
+            Console.WriteLine((int)manutencaoEletrica.Type); //Conversão explicita
+            Console.WriteLine(manutencaoEletrica.Type);
         }
     }
 
     struct Product
     {
-        public Product(int id, string name, double price)
+        public Product(int id, string name, double price, EProductType type)
         {
             Id = id;
             Name = name;
             Price = price;
+            Type = type;
         }
 
         public int Id;
         public string Name;
         public double Price;
+        public EProductType Type;
 
         public double PrinceInDolar(double dolar)
         {
             return Price * dolar;
         }
+    }
+
+    enum EProductType {
+        Product = 1,
+        Service = 2
     }
 }
