d6cac098e089ef8913ccc3553e4291210903fd40
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-24 10:28:32 -0300

    CSharp

---
+++ Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GuidExemplo();
+        }
+
+        static void GuidExemplo()
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("a55295ce-0417-40f1-be1c-0d7200785af5");
+
+            Console.WriteLine(id.ToString().Substring(0, 8));
+        }
+    }
+}
