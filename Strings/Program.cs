e0a30c6d68ceb738440ed916b869fb6714221ff7
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-24 10:59:06 -0300

    CSharp

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -6,7 +6,7 @@ namespace Strings
     {
         static void Main(string[] args)
         {
-            GuidExemplo();
+            InterpolaçãoStrings();
         }
 
         static void GuidExemplo()
@@ -17,6 +17,30 @@ namespace Strings
             id = new Guid("a55295ce-0417-40f1-be1c-0d7200785af5");
 
             Console.WriteLine(id.ToString().Substring(0, 8));
+        }
+
+        static void InterpolaçãoStrings()
+        {
+
+            /*
+                //Primeira forma de concatenação
+                var price = 10.2;
+                var texto = "O preço do produto é " + price + " apenas na promoção!";
+                Console.WriteLine(texto);
+            */
+
+            /*
+                //Segunda forma de concatenação
+                var price = 10.2;
+                var texto = string.Format("O preço do produto é {0} apenas na promoção!", price);
+                Console.WriteLine(texto);
+            */
+
+            //Terceira forma de concatenação - Pode usar @ depois do $ para escrever multiplas linhas de texto.
+            var price = 10.2;
+            var texto = $"O preço do produto é {price} apenas na promoção!";
+            Console.WriteLine(texto);
+
         }
     }
 }
