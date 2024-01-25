e6b3bb283668b85ba225e3f2d9c3a692e55d0d4e
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-25 09:50:20 -0300

    CSharp

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -45,27 +45,39 @@ namespace Strings
 
         static void ComparacaoStrings()
         {
-            /*
-                var texto = "Este texto é um teste";
-                Console.WriteLine(texto.CompareTo("Testando"));
 
-                Console.WriteLine(texto.CompareTo("testando"));
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.CompareTo("Testando"));
 
-                Console.WriteLine(texto.Contains("texto"));
+            Console.WriteLine(texto.CompareTo("testando"));
 
-                Console.WriteLine(texto.Contains("Texto"));
+            Console.WriteLine(texto.Contains("texto"));
 
-                Console.WriteLine(texto.Contains("Texto", StringComparison.OrdinalIgnoreCase));//Ignorando o case sensitive
+            Console.WriteLine(texto.Contains("Texto"));
 
-                Console.WriteLine(texto.StartsWith("Este"));//Verifica se a palavra inicial é a palavra escolhida
+            Console.WriteLine(texto.Contains("Texto", StringComparison.OrdinalIgnoreCase));//Ignorando o case sensitive
 
-                Console.WriteLine(texto.EndsWith("teste"));//Verifica se a palavra final é a palavra escolhida
+            Console.WriteLine(texto.StartsWith("Este"));//Verifica se a palavra inicial é a palavra escolhida
 
-                Console.WriteLine(texto.Equals("Este texto é um teste"));//Verifica se o texto escolhido é igual ao texto de comparação.
-            */
+            Console.WriteLine(texto.EndsWith("teste"));//Verifica se a palavra final é a palavra escolhida
 
-            var texto = "Esse texto é um teste";
-            Console.WriteLine(texto.IndexOf("é"));//Descobrir em qual indice(index) está o valor escolhido
+            Console.WriteLine(texto.Equals("Este texto é um teste"));//Verifica se o texto escolhido é igual ao texto de comparação.
+
+
+            var texto2 = "Esse texto é um teste";
+            Console.WriteLine(texto2.IndexOf("é"));//Descobrir em qual indice(index) está o valor escolhido
+
+            Console.WriteLine(texto2.LastIndexOf("s"));//Descobre em qual indice(index) esse valor apareceu por ultimo
+
+            Console.WriteLine(texto2.ToLower());//Convertendo toda a string para minuscula
+
+            Console.WriteLine(texto2.ToUpper());//Convertendo toda a string para maiuscula
+
+            Console.WriteLine(texto2.Insert(5, "AQUI "));//Inserindo um valor no local escolhido, index 5 = AQUI
+
+            Console.WriteLine(texto2.Remove(5, 5));//Remove a partir do index escolhido e os proximos valores até o numero escolhido.
+
+            Console.WriteLine(texto2.Length);//Diz a quantidade de itens na string
 
 
         }
