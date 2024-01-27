28a1dc1202a37b3db5ffdc2d6949869f6ef5b0f1
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-27 08:52:55 -0300

    CSharp

---
+++ Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var meuArray = new int[5] { 1, 2, 3, 4, 5 }; //Montagem de um Array(lista)
+
+            meuArray[0] = 12; //Adicionando um valor dentro do array escolhido
+
+            //Console.WriteLine(meuArray[0]);
+            //Console.WriteLine(meuArray[1]);
+            //Console.WriteLine(meuArray[2]);
+            //Console.WriteLine(meuArray[3]);
+            //Console.WriteLine(meuArray[4]);
+            Console.WriteLine(meuArray.Length);
+
+
+            var meuArrayString = new string[2] { "Maicon", "Mirela" };
+            Console.WriteLine(meuArrayString[0]);
+            Console.WriteLine(meuArrayString[1]);
+        }
+    }
+}
