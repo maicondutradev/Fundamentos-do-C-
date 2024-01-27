fe8065db59c4b42a71b2db933b96030d8242eed4
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-27 11:02:40 -0300

    CSharp

---
+++ Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[3];
+
+            try
+            {
+                for (var index = 0; index < 10; index++)
+                {
+                    Console.WriteLine(arr[index]);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                
+                Console.WriteLine("Ops, algo deu errado!");
+            }
+
+
+
+        }
+    }
+}
